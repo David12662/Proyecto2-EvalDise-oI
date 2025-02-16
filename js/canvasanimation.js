5867b2a2c5b0d4893d4dec359a986b660ee57c9f
David Devis Tamarit <davidevis1266@gmail.com>
2025-02-16 07:25:45 +0100

    Poyecto Terminado

--- js/canvasanimation.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener('DOMContentLoaded', function () {
-    var cartOffcanvas = document.getElementById('cartOffcanvas');
-    cartOffcanvas.addEventListener('shown.bs.offcanvas', function () {
-        cartOffcanvas.classList.add('fade-in');
-    });
-});
-
